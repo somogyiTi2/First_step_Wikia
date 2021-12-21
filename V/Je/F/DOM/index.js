3a136d7b44a946a010bbd76940f3713cd3e0a115
Somogyi Tibor <91289529+somogyiTi2@users.noreply.github.com>
2021-12-21 14:25:11 +0100

    Add files via upload

---
+++ V/Je/F/DOM/index.js
@@ -0,0 +1,4 @@
+
+document/*dokumentban*/.querySelector("h1")/*rákeres a h1-re*/
+.innerHTML = "<em>Good By</em>";/*megváltoztatja a HTML kódot majd kicseréli*/
+/*az <em></em>-el scirpten beül is dönthetem a szöveget.*/
