a477eaf8d26a2d54664e45a6dfca0095eb17bc3b
Somogyi Tibor <91289529+somogyiTi2@users.noreply.github.com>
2022-06-03 08:05:30 +0200

    Add files via upload

---
+++ php/hello.php
@@ -0,0 +1,167 @@
+<?php //Nyitórész
+
+$message = "Hello again!";//FONTOS! HA NEM ZÁROD LE A KÖVI SOR NAM MŰKÖDIK!
+$message2 = "Blabla!";
+//echo $message;
+//echo "Hello Word!";//echo kinyomja az üzenetet
+var_dump($message." ".$message2);//eggyesíti
+//$változó deffiniálása
+//nem lehet benne - jel
+$szam=5;
+$lebegosszam=3.14;
+var_dump($szam*$lebegosszam);//összeszrozza
+
+$data = null;
+
+var_dump($message);//megnézhető vele egy változó milyen értéket tartalmaz
+var_dump($szam);
+var_dump($lebegosszam);
+
+var_dump($data);
+
+$szovegeszam="450";
+$szoveg_mega_szam= $szovegeszam * $szam;
+
+var_dump($szoveg_mega_szam);//int 2250
+
+/*$szoveg_mega_szam2= $message * $szam;
+var_dump($szoveg_mega_szam2);//error*/
+
+$bool= true;
+var_dump(! $bool);//ellentétére fordítja a ! jel ...
+$boolf=false;
+var_dump($bool or $boolf);//true
+
+$napok= "hétfő\nkedd\nszerda\n";
+echo($napok);
+echo "szia " . $message2;
+echo "szia {$message2} ";
+
+$araylist=["első","második","stb"];
+//echo($araylist); NEM LEHET ECHO
+var_dump($araylist[1]);//0 VAL KEZDŐDIK
+var_dump($araylist[9]);//HA NINCS BENNE ERROR  +NULL
+
+$araylist=[6=>"első",5=>"második","pista"=>"stb"];
+var_dump($araylist[5]);
+var_dump($araylist['pista']);
+
+$osszekapcsoltTomb =[
+[
+  "name" =>"Alex",
+  "magas"=> 1.80,
+  "neme"=>"srác"
+],[
+  "name" =>"Luca",
+  "magas"=>1.75,
+  "neme"=>"lány"
+], [
+  "name" =>"Béla",
+  "magas"=>2.10,
+  "neme"=>"srác"
+]
+];
+var_dump($osszekapcsoltTomb);
+echo("-----válasz--------");
+var_dump($osszekapcsoltTomb[1]["neme"]);//lány
+
+
+foreach($araylist as $araylist){//elsőnek a tömb madj a változó neve ami minden elemhez hozzá lesz rendelve
+  echo $araylist, ", ";// it mindegy hogy közte , vagy . van ???
+}//a klódbok végén nem kell ;
+
+echo("-----válasz--------");
+
+$osszerakashozPelda=["eslő","második","stb"];
+
+foreach ($osszerakashozPelda as $index => $osszerakashozPelda) {
+  echo $index . '-'. $osszerakashozPelda.",";
+}
+echo("-----válasz--------");
+// FONTOS ha közben ugyan azon a néven iktatok be egy változót nem szól viszont kicsréli
+$osszerakashozPelda=[
+  'a'=>"eeeelső",
+  'b'=>"mmmmmásodik",
+  'c'=>"sssstb"
+];
+foreach ($osszerakashozPelda as $index => $osszerakashozPelda) {
+  echo $index . '-'. $osszerakashozPelda.",";
+};
+
+if(true){
+  echo ("true");
+};//alapvetően igaz ha nincs állítás, hozzáfogva
+
+$ures=[];
+
+var_dump(empty($ures));//truebool(true)
+//empty=üres
+
+if(empty($ures)){
+  echo ("Igen üres");
+}else{
+  echo ("Nem üres");
+}
+echo("--------------");
+
+var_dump(3==3);//true
+//== egyenlő
+//=!nem egyenlő
+//< kisebb nagyobb >
+//<= kisebb nagyobb vagy egyenlő >=
+
+$ho=1;
+while ($ho<=12){
+  echo $ho .",";
+  $ho=$ho+1;
+}
+
+echo ("WHILE vége után az értéke ho nak="). $ho.("/\n/");
+
+for ($i=1;$i<=10; $i++){//++$i így is lehet
+  echo $i.",";
+}
+echo ("FOR véges után az értéke ho nak = "). $i.("= illetve még i létezik...\n");
+
+$time=11;
+
+if ($time<12){
+  echo "Jóóóóóóóóóóóóóóó Reggelt Vietnám!";
+} elseif ($time<18){
+  echo "Jó Napot";
+} elseif($time<22){
+  echo "Szép jó estét";
+} else{
+  echo "Éjfélig minden ok";
+};
+
+echo("--------");
+
+$nap='SZo';
+
+switch ($nap) {
+  case 'H':
+    echo "Hétfő";
+    break;
+  case 'K':
+      echo "Kedd";
+    break;
+  case 'SZ':
+      echo "Szeda";
+      break;
+  case 'Cs':
+      echo "Csütörtök";
+      break;
+    case 'P':
+      echo "Péntek";
+    break;
+default:
+  echo "Nem tom mi van...";
+  break;
+}
+echo "<h1>SZIA MIZU ITT ÍGY MÜKSZIK IS A HTML CSAK SORONKÉNT KELL ÍRNI NEKI HOGY</h1><p>
+vagy is csak ha valamit kodosan is szét akarunk integrálni...
+</p>";
+echo"<p>
+echo
+</p>";
